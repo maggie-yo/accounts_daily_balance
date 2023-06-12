1adc206048fc5a6bd3c6c298a945adc34a73ecb4
Maggie Yonova <magdalena.yonova@gmail.com>
2023-06-12 19:26:50 +0000

    renamed table

--- models/marts/accounts_last_known_balance.sql
+++ models/marts/accounts_last_known_balance.sql
@@ -9,7 +9,7 @@ balance_day
 , account_id
 , cast(account_created_at as date) as account_created_date
 , balance_dkk as last_known_balance
-from {{ref ( 'int_appdb__accounts_with_balance' )}}
+from {{ref ( 'int_appdb__accounts_overview' )}}
 
 -- to get the last known balance per account
 qualify max(balance_day) over(partition by account_id)=balance_day
